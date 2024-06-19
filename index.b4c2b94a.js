110dbf0a98205fdd63635bc891671ef5ed2993ba
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-06-19 16:15:41 +0200

    make build

---
+++ index.b4c2b94a.js
@@ -0,0 +1,2 @@
+document.querySelector(".form__button").addEventListener("click",function(e){e.preventDefault()}),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add(".menu"):document.body.classList.remove(".menu")});
+//# sourceMappingURL=index.b4c2b94a.js.map
